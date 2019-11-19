1fc44f4dfd8938553ad956c0bfb6c9ed2810f234
Louison Vincent <lvincent009@iutbayonne.univ-pau.fr>
2019-11-19 13:50:38 +0100

    Création des pages index, entreprises, formations et stages avec l'objet Response

--- src/Controller/ProstageController.php
+++ src/Controller/ProstageController.php
@@ -4,26 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProstageController extends AbstractController
 {
     public function index()
     {
-        return $this->render('prostage/index.html.twig');
+        return new Response('<html><body><h1>Bienvenue sur la page d\'accueil de Prostages</h1></body></html>');
     }
 
     public function entreprises()
     {
-
+        return new Response('<html><body><h1>Cette page affichera la liste des entreprises proposant un stage</h1></body></html>');
     }
 
     public function formations()
     {
-        
+        return new Response('<html><body><h1>Cette page affichera la liste des formations de l\'IUT</h1></body></html>');
     }
 
-    public function stages()
+    public function stages($id)
     {
-        
+        return new Response('<html><body><h1>Cette page affichera le descriptif du stage ayant pour identifiant '.$id.'</h1></body></html>');
     }
 }
